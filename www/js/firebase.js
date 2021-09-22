2c1bd044fa7e1ac2768856d7ffe0ab910ca7e4fc
LucasLima2 <lucas.ana2@etec.sp.gov.br>
2021-09-21 23:00:37 -0300

    first commit

---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDo3gmRjgFCYUzOk-ojMJwwRq0nn0PLsfI",
+    authDomain: "pwiii-b5d8b.firebaseapp.com",
+    projectId: "pwiii-b5d8b",
+    storageBucket: "pwiii-b5d8b.appspot.com",
+    messagingSenderId: "321439924282",
+    appId: "1:321439924282:web:070b589d82371927113503",
+    measurementId: "G-ZPKV1LLVKD"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
